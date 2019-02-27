91bf7caeeb294ab32c4faf3489f0dd4b21d8a5e9
Kaushil R <mail2techboy@gmail.com>
2019-02-27 15:34:01 +0530

    Add files via upload

--- server.php
+++ server.php
@@ -1,12 +1,11 @@
 <?php
 /**
  * [getuserQuery Used to store user query in session for future use]
- * @return [type] [description]
+ * 
  */
 function getuserQuery() {
 	session_start();
 	$_SESSION['urllist'] = $_POST;
-	echo session_id()
 }
  
 getuserQuery();
